3be16cd99c092ad2dcae68e25a934cd5d293a012
Tathagat017 <114250830+Tathagat017@users.noreply.github.com>
2023-08-05 11:11:26 +0530

    changed error

--- set1/set1Practice2.py
+++ set1/set1Practice2.py
@@ -1,33 +1,24 @@
+var1 = 10
 
-variable1 = 10
+var2 = 3.14
 
+var3 = "Hello, Python!"
 
-variable2 = 3.14
+var4 = True
 
+var5 = [1, 2, 3, 4, 5]
 
-variable3 = "Hello, Python!"
+var6 = (6, 7, 8, 9, 10)
 
+var7 = {'name': 'John', 'age': 30, 'city': 'New York'}
 
-variable4 = True
+var8 = {1, 2, 3, 4, 5}
 
-
-variable5 = [1, 2, 3, 4, 5]
-
-
-variable6 = (6, 7, 8, 9, 10)
-
-
-variable7 = {'name': 'John', 'age': 30, 'city': 'New York'}
-
-
-variable8 = {1, 2, 3, 4, 5}
-
-
-print(f"Type of variable1: {type(variable1)}, value: {variable1}")
-print(f"Type of variable2: {type(variable2)}, value: {variable2}")
-print(f"Type of variable3: {type(variable3)}, value: {variable3}")
-print(f"Type of variable4: {type(variable4)}, value: {variable4}")
-print(f"Type of variable5: {type(variable5)}, value: {variable5}")
-print(f"Type of variable6: {type(variable6)}, value: {variable6}")
-print(f"Type of variable7: {type(variable7)}, value: {variable7}")
-print(f"Type of variable8: {type(variable8)}, value: {variable8}")
+print(f"Type of variable1: {type(var1)}, value: {var1}")
+print(f"Type of variable2: {type(var2)}, value: {var2}")
+print(f"Type of variable3: {type(var3)}, value: {var3}")
+print(f"Type of variable4: {type(var4)}, value: {var4}")
+print(f"Type of variable5: {type(var5)}, value: {var5}")
+print(f"Type of variable6: {type(var6)}, value: {var6}")
+print(f"Type of variable7: {type(var7)}, value: {var7}")
+print(f"Type of variable8: {type(var8)}, value: {var8}")
